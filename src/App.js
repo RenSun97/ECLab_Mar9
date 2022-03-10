1df969914dc781b92c8ba45b76421b6163dd6ce2
RenSun97 <71573447+RenSun97@users.noreply.github.com>
2022-03-09 22:44:18 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div id="grid">
+      <header>
+        <h1>Ren Sun</h1>
+      </header>
+      <div id="left-sidebar"></div>
+      <div id="content">
+        <div id="box"></div>
+      </div>
+      <div id="right-sidebar"></div>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
